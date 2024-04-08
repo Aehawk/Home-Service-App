67799482d3ad656e906802d220642cbfb382f3a4
Ahmet Ertuğrul Şahin <aes.bjk@gmail.com>
2024-04-08 15:35:44 +0300

    Home Service App first commit with auth and navigation bar

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './App/Screens/LoginScreen/Login';
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
+import * as SecureStore from "expo-secure-store";
+import { NavigationContainer } from '@react-navigation/native';
+import TabNavigation from './App/Navigations/TabNavigation';
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ClerkProvider
+      tokenCache={tokenCache}
+      publishableKey='pk_test_cXVpY2stYnVnLTU1LmNsZXJrLmFjY291bnRzLmRldiQ'
+    >
+      <View style={styles.container}>
+
+        {/* Sign In Component */}
+        <SignedIn>
+          <NavigationContainer>
+            <TabNavigation />
+          </NavigationContainer>
+        </SignedIn>
+
+        {/* Sign Out Component */}
+        <SignedOut>
+          <Login />
+        </SignedOut>
+
+        <StatusBar style="auto" />
+      </View>
+    </ClerkProvider>
   );
 }
 
@@ -14,7 +53,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 20,
   },
 });
