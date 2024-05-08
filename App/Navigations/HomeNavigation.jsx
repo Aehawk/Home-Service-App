9043a569b6f210d0583e6b40dec6325fde88bf2a
Ahmet Ertuğrul Şahin <aes.bjk@gmail.com>
2024-05-08 19:29:23 +0300

    Added Business Details Screen

--- App/Navigations/HomeNavigation.jsx
+++ App/Navigations/HomeNavigation.jsx
@@ -3,13 +3,15 @@ import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react'
 import HomeScreen from '../Screens/HomeScreen/HomeScreen';
 import BusinessListByCategoryScreen from '../Screens/BusinessListByCategoryScreen/BusinessListByCategoryScreen';
+import BusinessDetailsScreen from '../Screens/BusinessDetailsScreen/BusinessDetailsScreen';
 
 const Stack = createStackNavigator();
 export default function HomeNavigation() {
     return (
-        <Stack.Navigator screenOptions={{headerShown: false}}>
-            <Stack.Screen name='home' component={HomeScreen} />
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name='home-screen' component={HomeScreen} />
             <Stack.Screen name='business-list' component={BusinessListByCategoryScreen} />
+            <Stack.Screen name='business-details' component={BusinessDetailsScreen} />
         </Stack.Navigator>
     )
 }
--- App/Screens/BusinessListByCategoryScreen/BusinessListItem.jsx
+++ App/Screens/BusinessListByCategoryScreen/BusinessListItem.jsx
@@ -1,11 +1,13 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
 import Colors from '../../Utils/Colors'
 import Entypo from '@expo/vector-icons/Entypo';
+import { useNavigation } from '@react-navigation/native';
 
 export default function BusinessListItem({ business }) {
+    const navigation = useNavigation();
     return (
-        <View style={styles.container}>
+        <TouchableOpacity style={styles.container} onPress={() => navigation.push('business-details', { business: business })}>
             <Image source={{ uri: business?.images[0]?.url }}
                 style={styles.image} />
             <View style={styles.subContainer}>
@@ -16,7 +18,7 @@ export default function BusinessListItem({ business }) {
                     {business.address}
                 </Text>
             </View>
-        </View>
+        </TouchableOpacity>
     )
 }
 
