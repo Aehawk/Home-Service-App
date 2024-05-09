1cdcf97ff08e40f758f388717b55746ffe45f568
Ahmet Ertuğrul Şahin <aes.bjk@gmail.com>
2024-05-09 20:48:53 +0300

    Added Booking Screen

--- App/Screens/BusinessListByCategoryScreen/BusinessListItem.jsx
+++ App/Screens/BusinessListByCategoryScreen/BusinessListItem.jsx
@@ -3,22 +3,48 @@ import React from 'react'
 import Colors from '../../Utils/Colors'
 import Entypo from '@expo/vector-icons/Entypo';
 import { useNavigation } from '@react-navigation/native';
+import { AntDesign } from '@expo/vector-icons';
 
-export default function BusinessListItem({ business }) {
+export default function BusinessListItem({ business, booking }) {
     const navigation = useNavigation();
     return (
         <TouchableOpacity style={styles.container} onPress={() => navigation.push('business-details', { business: business })}>
             <Image source={{ uri: business?.images[0]?.url }}
                 style={styles.image} />
             <View style={styles.subContainer}>
-                <Text style={{ fontFamily: 'outfit', color: Colors.GRAY, fontSize: 15 }}>{business.contactPerson}</Text>
-                <Text style={{ fontFamily: 'outfit-medium', fontSize: 18 }}>{business.name}</Text>
-                <Text style={{ fontFamily: 'outfit', color: Colors.GRAY, fontSize: 13 }}>
-                    <Entypo name="location-pin" size={24} color={Colors.PRIMARY} style={{ marginRight: 10 }} />
-                    {business.address}
-                </Text>
+
+                <Text style={{ fontFamily: 'outfit', color: Colors.GRAY, fontSize: 15 }}>{business?.contactPerson}</Text>
+
+                <Text style={{ fontFamily: 'outfit-bold', fontSize: 19 }}>{business?.name}</Text>
+
+                {
+                    !booking?.id
+                        ? <Text style={{ fontFamily: 'outfit', color: Colors.GRAY, fontSize: 13 }}>
+                            <Entypo name="location-pin" size={24} color={Colors.PRIMARY} />
+                            {business.address}
+                        </Text>
+                        : <Text style={[{ padding: 5, borderRadius: 5, fontSize: 14, alignSelf: 'flex-start' },
+                            booking?.bookingStatus == 'Completed'
+                                ? { backgroundColor: Colors.LIGHT_GREEN, color: Colors.GREEN }
+                                : null,
+                            booking?.bookingStatus == 'Cancelled'
+                                ? { backgroundColor: Colors.LIGHT_RED, color: Colors.RED }
+                                : null,
+                            { color: Colors.PRIMARY, backgroundColor: Colors.PRIMARY_LIGHT }]}>
+                                {booking?.bookingStatus}
+                            </Text>
+                }
+
+                {
+                    booking?.id
+                        ? <Text style={{ fontFamily: 'outfit', color: Colors.GRAY, fontSize: 16 }}>
+                            <AntDesign name="calendar" size={20} color={Colors.PRIMARY} style={{ marginRight: 10 }} />
+                            {booking.date} at {booking.time}
+                        </Text>
+                        : null
+                }
             </View>
-        </TouchableOpacity>
+        </TouchableOpacity >
     )
 }
 
--- App/Utils/Colors.js
+++ App/Utils/Colors.js
@@ -5,4 +5,8 @@ export default {
     LIGHT_GRAY: '#EDEDED',
     PRIMARY_LIGHT: '#F1DEFF',
     GRAY: '#7f7f7f',
+    RED: '#FF0000',
+    LIGHT_RED: '#FFCCCB',
+    GREEN: '#00FF00',
+    LIGHT_GREEN: '#90EE90',
 }
